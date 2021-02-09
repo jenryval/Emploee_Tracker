3ba27a8fa92551e28fb6561a1c14ae2813690405
jenry valdes <jenryval93@gmail.com>
2021-02-08 20:44:42 -0600

    added server

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+const fs = require('fs')
 
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'Employee_db'
+});
+ 
+
+connection.connect();
+
+
+
+inquirer.prompt({
+
+})
