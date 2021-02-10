f0b58a52f872a3491101198127d914dc2cf116ac
jenry valdes <jenryval93@gmail.com>
2021-02-09 18:12:53 -0600

    added responses

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const fs = require('fs')
 
 
 var mysql      = require('mysql');
+const { allowedNodeEnvironmentFlags } = require('process');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -13,10 +14,41 @@ var connection = mysql.createConnection({
 });
  
 
-connection.connect();
+connection.connect(err => {
+  if (err) throw err
+  console.log(`connected to mysql on thread ${connection.threadId}`)
+});
 
 
 
-inquirer.prompt({
 
-})
+const employeeTracker = () =>
+    inquirer.prompt(
+  {
+    type:"list",
+    Name:"empolyee",
+    message:"What would you like to do?",
+    choices:['add departments', 'view departments', 'add empolyees', 'view employees', 'update employees', 'add roles', 'view roles']
+  },
+  
+  ).then(response => { 
+     if (response.employee === 'add departments') {
+      addDepartment()
+    } else if (response.employee === 'view departments') {
+      viewDepartment()
+    }else if (response.employee === 'add empolyees') {
+      addEmployee()
+    }else if (response.employee === 'view employees') {
+      viewEmployee()
+    }else if(response.employee === 'update employees') {
+      updateEmployee()
+    }else if (response.employee === 'add roles' ) {
+      addRole()
+    }else (response.employee === 'view roles') {
+      viewRoles()
+    };
+    
+  })
+  
+  const addDepartment()
+  
